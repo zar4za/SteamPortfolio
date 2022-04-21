4ce05d11690e6208eeab9179a9f89928fbe698c6
zar4za <zar4z4@yandex.ru>
2022-04-21 19:34:11 +0300

    Добавьте файлы проекта.

---
+++ SteamPortfolio/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SteamPortfolio.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private const string OpenIdProvider = "Steam";
+
+        [HttpGet("/signin")]
+        public IActionResult SignIn()
+        {
+            var properties = new AuthenticationProperties
+            {
+                RedirectUri = "/identity",
+            };
+
+            return Challenge(properties, OpenIdProvider);
+        }
+
+        [HttpGet("/logout")]
+        public IActionResult LogOut()
+        {
+            var properties = new AuthenticationProperties
+            {
+                RedirectUri = "/identity",
+            };
+
+            return SignOut(properties, CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
---
+++ SteamPortfolio/Program.cs
@@ -0,0 +1,39 @@
+using AspNet.Security.OpenId.Steam;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+.AddCookie(options =>
+{
+    options.LoginPath = "/login";
+    options.LogoutPath = "/signout";
+})
+.AddSteam();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseAuthentication();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
+
+app.Run();
